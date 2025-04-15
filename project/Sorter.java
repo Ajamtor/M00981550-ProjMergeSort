a08138de418def652129a287402f5aac5564a64c
Ajamtor <mehdiajami99@gmail.com>
2025-04-15 18:40:45 +0100

    Final file for sorter, code will do the actual sorting

--- project/Sorter.java
+++ project/Sorter.java
@@ -5,3 +5,41 @@ public class Sorter {
         if (list.size() <= 1) {
             return list;
         }
+        
+        int mid = list.size() / 2;
+        ArrayList<String> left = new ArrayList<>(list.subList(0, mid));
+        ArrayList<String> right = new ArrayList<>(list.subList(mid, list.size()));
+        
+        left = mergeSort(left);
+        right = mergeSort(right);
+        
+        return merge(left, right);
+    }
+    
+    private ArrayList<String> merge(ArrayList<String> left, ArrayList<String> right) {
+        ArrayList<String> result = new ArrayList<>();
+        int i = 0, j = 0;
+        
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i).compareToIgnoreCase(right.get(j)) <= 0) {
+                result.add(left.get(i));
+                i++;
+            } else {
+                result.add(right.get(j));
+                j++;
+            }
+        }
+        
+        while (i < left.size()) {
+            result.add(left.get(i));
+            i++;
+        }
+        
+        while (j < right.size()) {
+            result.add(right.get(j));
+            j++;
+        }
+        
+        return result;
+    }
+}
